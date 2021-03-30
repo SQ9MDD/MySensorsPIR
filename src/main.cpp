1a7e944fe68bab99f0a842f7cf4d7ded8ddd527b
Rysiek Labus <rysieklabus@gmail.com>
2021-03-30 20:47:37 +0200

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ MyMessage msgBI1(CHILD_ID_BI1, V_STATUS);
 void presentation(){
     char etykieta[] = "       ";
     int addr = MY_NODE_ID;  
-    sendSketchInfo("AS-200R-DG", "1.1");
+    sendSketchInfo("MySensorsPIR", "1.1");
     sprintf(etykieta,"R%02u.AI1",addr);  present(CHILD_ID_AI1, S_MULTIMETER, etykieta);  
     sprintf(etykieta,"R%02u.BI1",addr);  present(CHILD_ID_BI1, S_BINARY, etykieta);  
 }
@@ -33,17 +33,13 @@ void setup(){
     analogReference(INTERNAL);
     pinMode(BATTERY_SENSE_PIN,INPUT);
     pinMode(BI1_INPUT,INPUT_PULLUP);   
-    //sleep(60000);
     avg_mesure = analogRead(BATTERY_SENSE_PIN);
 }
 
 void loop(){
+    // read data
     int sensorValue = analogRead(BATTERY_SENSE_PIN);
     bool bi1_state = digitalRead(BI1_INPUT) == HIGH;
-    
-    // 1M8, 470K divider across battery and using internal ADC ref of 1.1V
-    // ((1e6+470e3)/470e3)*1.1 = Vmax
-    // Vmax/1023 = Volts per bit = 0.00502926
 
     // smoothing readings 
     avg_mesure = (avg_mesure*(weight-1) + sensorValue) / weight;
@@ -52,8 +48,12 @@ void loop(){
     float batteryPcnt = 100.0/835.0 * float(avg_mesure);
     batteryPcnt = constrain(batteryPcnt,0,100);
     float batteryV  = avg_mesure * 0.00502926;
+
+    // send data
     sendBatteryLevel(batteryPcnt);
     send(msgAI1.set(batteryV, 2)); 
     send(msgBI1.set(bi1_state,1));
-    sleep(digitalPinToInterrupt(BI1_INPUT), CHANGE, SLEEP_TIME);                                                      // go to sleep
+
+    // go to sleep
+    sleep(digitalPinToInterrupt(BI1_INPUT), CHANGE, SLEEP_TIME);                                                      
 }
