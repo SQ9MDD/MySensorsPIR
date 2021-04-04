74f17df292b9f9582d63915212b743c59162655f
Rysiek Labus <rysieklabus@gmail.com>
2021-04-04 17:54:10 +0200

    internall voltage mesure

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
 #include <Arduino.h>
 
-#define CHILD_ID_AI1        1             // mysensors first analog input child ID
-#define CHILD_ID_BI1        2             // mysensors binary input child ID
-#define BATTERY_SENSE_PIN   A0            // voltage mesure pin
-#define BI1_INPUT           3             // contactron input
+#define CHILD_ID_AI1        1               // mysensors first analog input child ID
+#define CHILD_ID_BI1        2               // mysensors binary input child ID
+#define BATTERY_SENSE_PIN   A0              // voltage mesure pin
+#define BI1_INPUT           3               // contactron input
 
 // setup radio
-#define MY_RADIO_NRF24
-#define MY_DEBUG                          // Enable debug prints
-#define MY_NODE_ID      43                // <--- !!! SET NODE ADDRESS HERE !!!
-#define MY_RF24_CHANNEL 80                // channel from 0 to 125. 76 is default
+#define MY_RADIO_NRF24                      // type of transceiver
+#define MY_DEBUG                            // Enable debug prints
+#define MY_NODE_ID      43                  // <--- !!! SET NODE ADDRESS HERE !!!
+#define MY_RF24_CHANNEL 80                  // channel from 0 to 125. 76 is default
 #include <MySensors.h>
              
-unsigned long SLEEP_TIME = 300000;        // default 5m sleep time between reads and data send (seconds * 1000 milliseconds)
-unsigned int weight = 5;                  // Max 64 min 1, unsigned int = 0-65535 max from adc 1023 *64 = 65472
-unsigned int avg_mesure = 0;
-MyMessage msgAI1(CHILD_ID_AI1, V_VOLTAGE);
-MyMessage msgBI1(CHILD_ID_BI1, V_STATUS);
+unsigned long SLEEP_TIME = 300000;          // default 5m sleep time between reads and data send (seconds * 1000 milliseconds)
+float weight = 5.0;                         // scale, filter for smoothing measurements
+float avg_mesure = 0.0;                     // variable for average value of the last measurements
+float sensorValue = 0.0;                    //
+MyMessage msgAI1(CHILD_ID_AI1, V_VOLTAGE);  //
+MyMessage msgBI1(CHILD_ID_BI1, V_STATUS);   //
 
 void presentation(){
     char etykieta[] = "        ";
@@ -29,30 +30,42 @@ void presentation(){
 void setup(){
     analogReference(INTERNAL);
     pinMode(BATTERY_SENSE_PIN,INPUT);
+
     #ifdef PIR_SENSOR
         pinMode(BI1_INPUT,INPUT); 
     #elif CONTACTRON_SENSOR
         pinMode(BI1_INPUT,INPUT_PULLUP); 
     #endif  
-    avg_mesure = analogRead(BATTERY_SENSE_PIN);
+
+    #ifdef INTERNAL_VOLT_MESURE
+        avg_mesure = hwCPUVoltage() / 1000.0;
+    #else
+        avg_mesure = analogRead(BATTERY_SENSE_PIN) * 0.00502926;
+    #endif
 }
 
 void loop(){
-    // read data
-    int sensorValue = analogRead(BATTERY_SENSE_PIN);
     bool bi1_state = digitalRead(BI1_INPUT);
+
+    #ifdef INTERNAL_VOLT_MESURE
+        sensorValue = hwCPUVoltage() / 1000.0;
+    #else
+        sensorValue = analogRead(BATTERY_SENSE_PIN) * 0.00502926;
+    #endif
 
     // smoothing readings 
     avg_mesure = (avg_mesure*(weight-1) + sensorValue) / weight;
     
-    // calculate data
-    float batteryPcnt = 100.0/835.0 * float(avg_mesure);
+    // calculate battery percent
+    // 3,4V - 0%
+    // 4,2V - 100%
+    float bat_low = 3.4;
+    float batteryPcnt = 100.0/(4.2-bat_low) * float(avg_mesure-bat_low);
     batteryPcnt = constrain(batteryPcnt,0,100);
-    float batteryV  = avg_mesure * 0.00502926;
 
     // send data
     sendBatteryLevel(batteryPcnt);
-    send(msgAI1.set(batteryV, 2)); 
+    send(msgAI1.set(avg_mesure, 2)); 
     send(msgBI1.set(bi1_state,1));
 
     // go to sleep
